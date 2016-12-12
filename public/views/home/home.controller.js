b7b0ec1c28fb1361baf4ef65d8d5e6e2f5dee9e0
Hadjir Mashiri <hmashiri@hotmail.com>
2016-12-12 10:06:43 -0800

    grid system for firm project

---
+++ public/views/home/home.controller.js
@@ -0,0 +1,12 @@
+(function()
+{
+    angular
+        .module("PassportApp")
+        .controller("HomeController", HomeController);
+    
+    function HomeController($scope)
+    {
+
+    }
+
+})();
