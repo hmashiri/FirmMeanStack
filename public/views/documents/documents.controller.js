ba3138cada785b5bc3059ce9b800ae052e994a8a
Hadjir Mashiri <hmashiri@hotmail.com>
2016-12-13 13:16:09 -0800

    saving point, added document download functionality

--- public/views/documents/documents.controller.js
+++ public/views/documents/documents.controller.js
@@ -3,7 +3,8 @@
     angular
         .module("PassportApp")
         .controller("DocumentsCtrl", DocumentsCtrl)
-        .factory("AttorneyImageService", AttorneyImageService);
+        .factory("AttorneyImageService", AttorneyImageService)
+        .factory("DownloadFileService", DownloadFileService);
 
     function AttorneyImageService( $http, $rootScope)
     {
@@ -32,15 +33,56 @@
         return this;
     }
 
-    function DocumentsCtrl($scope, $http, AttorneyImageService)
+    function DownloadFileService( $http, $rootScope, $q)
+    {
+        var service = {
+
+            downloadFile: downloadFile,
+
+        };
+        return service;
+
+
+        function  downloadFile(file_id, filename )
+        {
+            var defer = $q.defer();
+
+            $http
+            ({
+                method: 'GET',
+                url: 'http://localhost:3001/api/files/' + file_id,
+                responseType: 'arraybuffer'
+            })
+
+                .then (function(response)
+                {
+                    var file = new Blob([response.data]);
+
+                    saveAs(file, filename);
+                    defer.resolve(response);
+                },
+
+                function(error)
+                {
+                    defer.reject(error)
+                });
+
+                return defer.promise;
+        }
+    }
+
+
+
+    function DocumentsCtrl($scope, $http, DownloadFileService, AttorneyImageService)
     {
         var vm = this;
         vm.title = 'Controller';
         vm.test = 'TESTING WORKS';
 
         vm.imageValues = [];
-
+        vm.downloadOnClick = downloadOnClick;
         AttorneyImageService.retrieveImageInfo();
+
 
         $scope.$on('jpgsReceived', function (msg, data)
         {
@@ -50,6 +92,29 @@
         activate();
 
         function activate() {}
+
+        function downloadOnClick(file_id, filename)
+        {
+            console.log(file_id);
+
+            DownloadFileService.downloadFile(file_id, filename).then(function(response)
+
+            {
+                  console.log(response);
+            })
+
+        }
+
+        $scope.add = function(){
+            var f = document.getElementById('file').files[0],
+                r = new FileReader();
+            r.onloadend = function(e){
+                var data = e.target.result;
+                //send your binary data via $http or $resource or do anything else with it
+            };
+            r.readAsBinaryString(f);
+        }
+
     }
 
 })();
